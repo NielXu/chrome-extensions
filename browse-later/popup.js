06faf0faff85f5f672c3152a904ea7bce4c0ef89
Daniel Xu <niellxu@yahoo.ca>
2019-06-12 16:11:08 -0400

    Added more style, item will disappear after clicking them from browse list

--- browse-later/popup.js
+++ browse-later/popup.js
@@ -57,19 +57,64 @@ function renderlist(items) {
     }
     const ul = document.getElementById("list");
     ul.innerHTML = "";
+    let index = items.length - 1;
     while(items.length > 0) {
         const item = items.pop();
-        ul.innerHTML += "<li>\
-            <a href='"+item[0]+"' target='_newtab'>"+item[2]+"</a>\
-            </li>"
+        const li = document.createElement("li");
+        const img = document.createElement("img");
+        if(item[1]) {
+            img.setAttribute("src", item[1]);
+        }
+        else{
+            img.setAttribute("src", "images/default.png");
+        }
+        li.setAttribute("data-index", index);
+        li.appendChild(img);
+        const a = document.createElement("a");
+        a.onclick = function() {
+            remove(li.getAttribute("data-index"));
+        };
+        a.setAttribute("href", item[0]);
+        a.setAttribute("target", "_newtab");
+        a.setAttribute("title", item[2]);
+        const title = document.createTextNode(trimTitle(item[2]));
+        a.appendChild(title);
+        li.appendChild(a);
+        ul.appendChild(li);
+        index --;
     }
 }
 
+/**
+ * Load page and get data from google storage
+ */
 function loadpage() {
     chrome.storage.sync.get(["list"], function(data) {
         items = data.list;
         renderlist(items);
     })
+}
+
+/**
+ * Reduces the length of very long title
+ */
+function trimTitle(title) {
+    const maxlen = 45;
+    return title.length > maxlen? title.slice(0, maxlen) + "..." : title;
+}
+
+/**
+ * Remove the element from the list, also remove it from the storage
+ */
+function remove(index) {
+    chrome.storage.sync.get(["list"], function(data) {
+        const items = data.list;
+        items.splice(index, 1);
+        chrome.storage.sync.set({list: items}, function() {
+            // reload page after removing one item
+            loadpage()
+        })
+    });
 }
 
 
